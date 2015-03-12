2bedbef2a538e6679f401ba5f15f57e24df23056
Claudinei Matos <claudineimatos@gmail.com>
2015-03-12 14:54:17 -0300

    Fixed default permission (mode) which has to be four digit, not three Added suppor to create files from a remote url

--- manifests/createfile.pp
+++ manifests/createfile.pp
@@ -2,21 +2,56 @@ define filemanager::createfile(
   $path		= undef,
 	$owner	= 'root',
 	$group	= 'root',
-	$mode		= 600,
+	$mode		= 0600,
 	$content	= undef,
 	$replace = true,
+	$url	= undef
 ) {
 
-  validate_string($path)
+  	validate_string($path)
 	validate_string($content)
+	validate_string($url)
 	validate_bool($replace)
 
-	file { $path:
-		ensure 	=> 'file',
-		owner 	=> $owner,
-		group		=> $group,
-		mode		=> $mode,
-		content	=> $content,
-		replace => $replace,
+	if($url)
+	{
+
+		exec { "mkdir ${$path}":
+			path => '/usr/bin:/bin:/usr/sbin:/sbin',
+			command => "mkdir -p ${$path}",
+			unless => "test -d ${$path}"
+		}
+
+		exec { "download file ${name}":
+			path => '/usr/bin:/bin:/usr/sbin:/sbin',
+			command => "wget \"$url\" -O /tmp/$name",
+			require => exec["mkdir ${$path}"]
+		}
+
+		file { "${$path}/${$name}":
+			ensure 	=> 'file',
+			owner 	=> $owner,
+			group		=> $group,
+			mode		=> $mode,
+			source	=> "/tmp/$name",
+			replace => $replace,
+			require => exec["download file ${$name}"],
+		}
+
+		file { "/tmp/${$name}":
+			ensure => absent,
+			require => [ file["${path}/${$name}"], exec["download file ${$name}"] ]
+		}
+	}
+	else
+	{
+		file { "${$path}/${$name}":
+			ensure 	=> 'file',
+			owner 	=> $owner,
+			group		=> $group,
+			mode		=> $mode,
+			content	=> $content,
+			replace => $replace,
+		}
 	}
 }
